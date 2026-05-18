8ed237eb22689b623258f439847adf90537a53e4
DeveloperSky95 <haneulgim95@gmail.com>
2026-05-18 17:19:05 +0900

    adminCategoryListPage API 작성 완료

--- src/pages/admin/category/AdminCategoryListPage.tsx
+++ src/pages/admin/category/AdminCategoryListPage.tsx
@@ -1,4 +1,27 @@
+import { useEffect, useState } from "react";
+import adminCategoryApi from "../../../api/admin/adminCategoryApi.ts";
+import type { Category } from "../../../types/category.type.ts";
+
 function AdminCategoryListPage() {
+    const [categories, setCategories] = useState<Category[]>([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const loadCategories = async () => {
+            try {
+                const data = await adminCategoryApi.fetchCategoryList();
+                setCategories(data);
+            } catch (error) {
+                console.error(error);
+                alert("카테고리 목록을 불러오는데 실패했습니다.");
+            } finally {
+                setIsLoading(false);
+            }
+        }
+
+        loadCategories().then(() => {});
+    }, [])
+
     return <>fffff</>
 }
 
