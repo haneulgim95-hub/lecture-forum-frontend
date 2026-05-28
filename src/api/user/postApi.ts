ac3a76c2a9b640db53f17a04b0feb8db5c4db4b5
DeveloperSky95 <haneulgim95@gmail.com>
2026-05-28 13:20:08 +0900

    PostListPage, PostCreatePage 작성중.

--- src/api/user/postApi.ts
+++ src/api/user/postApi.ts
@@ -2,8 +2,8 @@ import axiosInstance from "../axiosInstance.ts";
 import type { PaginationResponseType } from "../../types/common.type.ts";
 import type { Post } from "../../types/post.type.ts";
 
-const fetchPostListByCategory = async (id: number): Promise<PaginationResponseType<Post>> => {
-    const response = await axiosInstance.get(`/post/list/${id}`);
+const fetchPostListByCategory = async (categoryId: number, page: number, size: number): Promise<PaginationResponseType<Post>> => {
+    const response = await axiosInstance.get(`/post/list/${categoryId}?page=${page}&size=${size}`);
     return response.data.data;
 };
 
