7f66faa250e08d4c49fc90e66e25d1ac447d3b08
DeveloperSky95 <haneulgim95@gmail.com>
2026-06-04 12:53:50 +0900

    PostReply.tsx 목록 기능 추가

--- src/components/post/PostReply.tsx
+++ src/components/post/PostReply.tsx
@@ -4,13 +4,24 @@ import {
     createReplySchema,
 } from "../../schemas/reply/createReplySchema.ts";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { ReplyContainer, ReplyForm, ReplyTitle } from "./reply.style.tsx";
+import {
+    EmptyMessage,
+    ReplyContainer,
+    ReplyContent,
+    ReplyForm,
+    ReplyHeader,
+    ReplyItem,
+    ReplyList,
+    ReplyTitle,
+} from "./reply.style.tsx";
 import { LuMessageSquare } from "react-icons/lu";
 import TextareaGroup from "../common/textarea/TextareaGroup.tsx";
 import Button from "../common/button/Button.tsx";
 import { useAuthStore } from "../../stores/auth/authStore.ts";
 import replyApi from "../../api/user/replyApi.ts";
-import { useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
+import type { Reply } from "../../types/reply.type.ts";
+import ReplyPagination from "../common/pagination/ReplyPagination.tsx";
 
 interface Props {
     postId: number;
@@ -18,7 +29,48 @@ interface Props {
 
 function PostReply({ postId }: Props) {
     // 사용자가 댓글 내용을 입력하고, 그걸 백엔드에게 저장해달라고 요청
-    const { isLoggedIn } = useAuthStore();
+    const { isLoggedIn, user } = useAuthStore();
+    const [list, setList] = useState<Reply[]>([]);
+
+    // 댓글 목록을 포함해야 하니, pagination도 해야되는구나. 근데 얠 querystring에 포함 시켜야되나?
+    // 사용자 목록 또는 글 목록을 할 떄에는 페이지네이션 정보를 쿼리스트링에 포함했었음
+    // 근데 지금 보면, PostReply라고 하는 컴포넌트는 PostDetailPage의 하위 컴포넌트로 구상되어져 있음
+    // 이 이야기는 지금 사용 중인 주소는 "PostDetailPage"가 출력되는 주소
+    // 결국 여기에서 쿼리스트링을 쓰면 /post/7?page=1&size=10 이러한 결과가 됨
+    // 이렇게 해도 됨
+    // 근데 주소를 가지고 값을 따져보기엔 모호해짐. 즉 이건 직관적이지 못하다.
+    // 그리고 쿼리스트링으로 관리하는 이유는 "뒤로가기" 때문에, 히스토리를 유지하기 위해 하는 것
+    // 여기에서의 내 판단은 "글"이 중심이 되어야지 "댓글"이 중심은 아니다. 쿼리스트링을 안 쓸 것임
+
+    const [page, setPage] = useState(1);
+    const [total, setTotal] = useState(0);
+    const size = 10;
+    const totalPage = Math.max(1, Math.ceil(total / size)); // 총 페이지 매 수 (total을 1로 대입해서 생각해보길..)
+    const [isLoading, setIsLoading] = useState(true);
+
+    const loadReplies = useCallback(
+        async (page: number) => {
+            // 함수 스코프에 의해서
+            // 이 함수가 갖게 되는 page 변수는 부모가 갖고 있는 page state가 아니라 page 매개변수가 됨
+            try {
+                const result = await replyApi.getRepliesByPostId(postId, page, size);
+                setList(result.list);
+                setTotal(result.total);
+                setPage(page);
+            } catch (error) {
+                console.log(error);
+                // 부속품인데 댓글목록을 불러오는데 실패했다고 얘를 목록을 보내는건 이상하다.
+            } finally {
+                setIsLoading(false);
+            }
+        },
+        [postId],
+    );
+
+    useEffect(() => {
+        // eslint-disable-next-line react-hooks/set-state-in-effect
+        loadReplies(1).then(() => {});
+    }, [loadReplies]);
 
     const {
         register,
@@ -48,10 +100,24 @@ function PostReply({ postId }: Props) {
         try {
             await replyApi.createReply(postId, data.content);
             reset(); // textarea에 값이 입력되어져 있는 상태이기 때문에 그걸 비우려고
-            // TODO : 댓글 목록을 다시 불러와야 함
+            // 1. 내가 댓글을 작성하면 실행
+            // 2. 글내용을 보면, 이미 댓글 목록도 불러와졌어야 함
+            await loadReplies(1);
         } catch (error) {
             console.log("댓글 작성 실패 : ", error);
             alert("댓글 작성 중 오류가 발생했습니다.");
+        }
+    };
+
+    const handleDeleteReply = async (replyId: number) => {
+        if (!confirm("정말 이 댓글을 삭제 하시겠습니까?")) return;
+
+        try {
+            await replyApi.deleteReply(replyId);
+            await loadReplies(1);
+        } catch (error) {
+            console.log("댓글 삭제 실패: ", error);
+            alert("댓글 삭제 중 오류가 발생되었습니다.");
         }
     };
 
@@ -84,6 +150,43 @@ function PostReply({ postId }: Props) {
                     {isSubmitting ? "등록 중..." : "댓글 등록"}
                 </Button>
             </ReplyForm>
+
+            <ReplyList>
+                {isLoading ? (
+                    <EmptyMessage>댓글을 불러오는 중입니다.</EmptyMessage>
+                ) : list.length === 0 ? (
+                    <EmptyMessage>가장 먼저 토론에 참여해보세요!</EmptyMessage>
+                ) : (
+                    list.map(item => (
+                        <ReplyItem key={item.id}>
+                            <ReplyHeader>
+                                <div className={"author-info"}>
+                                    <strong>{item.user.nickname}</strong>
+                                    <span className={"date"}>
+                                        {new Date(item.createdAt).toLocaleString("ko-kr", {
+                                            year: "numeric",
+                                            month: "2-digit",
+                                            day: "2-digit",
+                                            hour: "2-digit",
+                                            minute: "2-digit",
+                                        })}
+                                    </span>
+                                </div>
+                                {user?.id === item.userId && (
+                                    <button
+                                        className={"delete-btn"}
+                                        onClick={() => handleDeleteReply(item.id)}>
+                                        삭제
+                                    </button>
+                                )}
+                            </ReplyHeader>
+                            <ReplyContent>{item.content}</ReplyContent>
+                        </ReplyItem>
+                    ))
+                )}
+            </ReplyList>
+
+            <ReplyPagination currentPage={page} totalPage={totalPage} onPageChange={loadReplies} />
         </ReplyContainer>
     );
 }
